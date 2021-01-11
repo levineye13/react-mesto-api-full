4107c61dc1fa1ef25592cdeb24ac5f46104d16dc
Oleg Zharov <levineye@gmail.com>
2021-01-11 15:17:29 +0300

    feat: сделать имя, аватарку необязательными

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,22 +21,23 @@ const userSchema = new Schema({
   },
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (v) => regexp.test(v),
     },
+    default:
+      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
