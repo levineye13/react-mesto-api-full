c11775227ed93abfc31e3626723d4265483d880c
Oleg Zharov <levineye@gmail.com>
2021-01-13 17:55:24 +0300

    feat: добавить пользовательские ошибки

---
+++ backend/errors/errors.js
@@ -0,0 +1,11 @@
+const BadRequestError = require('./bad-request-error');
+const UnauthorizedError = require('./unauthorized-error');
+const NotFoundError = require('./not-found-error');
+const InternalServerError = require('./internal-server-error');
+
+module.exports = {
+  BadRequestError,
+  UnauthorizedError,
+  NotFoundError,
+  InternalServerError,
+};
