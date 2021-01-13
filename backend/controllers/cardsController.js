754b3485093cc67c55a4ffafe4179db126708d3d
Oleg Zharov <levineye@gmail.com>
2021-01-13 22:50:59 +0300

    feat: добавить централизованную обработку ошибок в контроллер карточек

--- backend/controllers/cardsController.js
+++ backend/controllers/cardsController.js
@@ -1,31 +1,19 @@
-const { Types, Model } = require('mongoose');
 const Card = require('./../models/card');
-const {
-  STATUS_OK,
-  BAD_REQUEST_ERROR,
-  NOT_FOUND_ERROR,
-  INTERNAL_SERVER_ERROR,
-} = require('./../utils/constants');
-const { handleError } = require('./../utils/utils');
+const { NotFoundError, BadRequestError } = require('./../errors/errors');
 
 /**
  * @param  {Object} req - объект запроса к серверу
  * @param  {Object} res - объект ответа сервера
  */
-const getCards = async (req, res) => {
+const getCards = async (req, res, next) => {
   try {
     const cards = await Card.find({});
     if (cards.length === 0) {
-      res.status(NOT_FOUND_ERROR).send({ message: 'Карточки не найдены' });
-      return;
+      throw new NotFoundError('Карточки не найдены');
     }
-    res.status(STATUS_OK).send(cards);
+    res.status(200).send(cards);
   } catch (err) {
-    handleError({
-      responce: res,
-      error: err,
-      errorCode: INTERNAL_SERVER_ERROR,
-    });
+    next(err);
   }
 };
 
@@ -34,23 +22,19 @@ const getCards = async (req, res) => {
  * @param  {Object} req - объект запроса к серверу
  * @param  {Object} res - объект ответа сервера
  */
-const createCard = async (req, res) => {
+const createCard = async (req, res, next) => {
   const { name, link } = req.body;
   const { _id } = req.user;
 
   try {
     const newCard = await Card.create({ name, link, owner: { _id } });
-    res.status(STATUS_OK).send(newCard);
+    res.status(200).send(newCard);
   } catch (err) {
-    handleError({
-      responce: res,
-      error: err,
-      //Если ошибка валидации, то 400, иначе 500
-      errorCode:
-        err.name === 'ValidationError'
-          ? BAD_REQUEST_ERROR
-          : INTERNAL_SERVER_ERROR,
-    });
+    next(
+      err.name === 'ValidationError'
+        ? new BadRequestError('Переданы некорректные данные')
+        : err
+    );
   }
 };
 
@@ -59,20 +43,17 @@ const createCard = async (req, res) => {
  * @param  {Object} req - объект запроса к серверу
  * @param  {Object} res - объект ответа сервера
  */
-const deleteCard = async (req, res) => {
+const deleteCard = async (req, res, next) => {
   const { cardId } = req.params;
 
   try {
     const deletedCard = await Card.findByIdAndRemove(cardId);
-    return !deletedCard
-      ? res.status(NOT_FOUND_ERROR).send({ message: 'Карточка не найдена' })
-      : res.status(STATUS_OK).send(deletedCard);
+    if (!deletedCard) {
+      throw new NotFoundError('Карточка не найдена');
+    }
+    res.status(200).send(deletedCard);
   } catch (err) {
-    handleError({
-      responce: res,
-      error: err,
-      errorCode: INTERNAL_SERVER_ERROR,
-    });
+    next(err);
   }
 };
 
@@ -81,7 +62,7 @@ const deleteCard = async (req, res) => {
  * @param  {Object} req - объект запроса к серверу
  * @param  {Object} res - объект ответа сервера
  */
-const likeCard = async (req, res) => {
+const likeCard = async (req, res, next) => {
   const { _id } = req.user;
   const { cardId } = req.params;
 
@@ -91,15 +72,12 @@ const likeCard = async (req, res) => {
       { $addToSet: { likes: _id } },
       { new: true }
     );
-    return !updatedCard
-      ? res.status(NOT_FOUND_ERROR).send({ message: 'Карточка не найдена' })
-      : res.status(STATUS_OK).send(updatedCard);
+    if (!updatedCard) {
+      throw new NotFoundError('Карточка не найдена');
+    }
+    res.status(200).send(updatedCard);
   } catch (err) {
-    handleError({
-      responce: res,
-      error: err,
-      errorCode: INTERNAL_SERVER_ERROR,
-    });
+    next(err);
   }
 };
 
@@ -108,7 +86,7 @@ const likeCard = async (req, res) => {
  * @param  {Object} req - объект запроса к серверу
  * @param  {Object} res - объект ответа сервера
  */
-const dislikeCard = async (req, res) => {
+const dislikeCard = async (req, res, next) => {
   const { _id } = req.user;
   const { cardId } = req.params;
 
@@ -118,15 +96,12 @@ const dislikeCard = async (req, res) => {
       { $pull: { likes: _id } },
       { new: true }
     );
-    return !updatedCard
-      ? res.status(NOT_FOUND_ERROR).send({ message: 'Карточка не найдена' })
-      : res.status(STATUS_OK).send(updatedCard);
+    if (!updatedCard) {
+      throw new NotFoundError('Карточка не найдена');
+    }
+    res.status(200).send(updatedCard);
   } catch (err) {
-    handleError({
-      responce: res,
-      error: err,
-      errorCode: INTERNAL_SERVER_ERROR,
-    });
+    next(err);
   }
 };
 
