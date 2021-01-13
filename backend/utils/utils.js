77f31f78e16dbf8efbbe0d16666f964f3edd4bab
Oleg Zharov <levineye@gmail.com>
2021-01-13 22:53:46 +0300

    refactor: убрать лишний код

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,10 +1,1 @@
-/**
- * Функция обработки ошибок
- * @param  {Object} responce - объект ответа сервера
- * @param  {Object} error - объект ошибки
- * @param  {Number} errorCode - код ошибки http запроса
- */
-module.exports.handleError = ({ responce, error, errorCode }) => {
-  console.error(error.stack);
-  responce.status(errorCode).send({ message: error.message });
-};
+
