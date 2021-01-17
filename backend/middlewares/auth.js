9a61bee63091f6b0177abfac0db586a7d55117eb
Oleg Zharov <levineye@gmail.com>
2021-01-17 18:27:21 +0300

    feat: обработка отсутствия авторизации через next

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,22 +4,23 @@ const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors/errors');
 
 module.exports = (req, res, next) => {
-  const { Authorization: authorization } = req.headers;
+  const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
 
   let payload;
+
   try {
     payload = jwt.verify(
       token,
       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
     );
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
