57a7666e3be4f9ef87af3a76da16286327ae7d92
Oleg Zharov <levineye@gmail.com>
2021-01-17 18:35:37 +0300

    feat: если карточек еще нет, сделать секундный лоадер после чего вывести надпись

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Card from './Card';
 import { CurrentUserContext } from './../contexts/CurrentUserContext';
 import Loader from '../components/Loader';
@@ -13,6 +13,11 @@ const Main = ({
   onCardLike,
 }) => {
   const currentUser = useContext(CurrentUserContext);
+  const [cardsLoaded, setCardsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setCardsLoaded(true), 1000);
+  }, []);
 
   return (
     <main className="content page__content">
@@ -39,17 +44,19 @@ const Main = ({
       <section className="elements content__elements">
         <ul className="elements__list">
           {cards.length > 0 ? (
-            cards.map((card) => {
-              return (
-                <Card
-                  card={card}
-                  onCardClick={onCardClick}
-                  onDeleteButtonClick={onDeleteButtonClick}
-                  onCardLike={onCardLike}
-                  key={card._id}
-                />
-              );
-            })
+            cards.map((card) => (
+              <Card
+                card={card}
+                onCardClick={onCardClick}
+                onDeleteButtonClick={onDeleteButtonClick}
+                onCardLike={onCardLike}
+                key={card._id}
+              />
+            ))
+          ) : cardsLoaded ? (
+            <p className="elements__baner">
+              Пока не создано ни одной карточки!
+            </p>
           ) : (
             <Loader />
           )}
