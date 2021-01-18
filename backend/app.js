09ed6012fd92a6c95f20826c5a02db77b4e38d10
Oleg Zharov <levineye@gmail.com>
2021-01-18 22:53:08 +0300

    feat: установить и подключить парсер кук

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const cors = require('cors');
 
