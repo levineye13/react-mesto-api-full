fec7c75fe39475ea6081796be4726b4a0f7ae88a
Oleg Zharov <levineye@gmail.com>
2021-01-29 00:20:58 +0300

    feat: изменить функцию разделения массива

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,9 @@
 /**
  * Данная функция берет массив, который вернул Promise.allSetled (с полями value и status)
- * и возвращает новый массив из полей value
+ * и возвращает новый массив из полей value, если промис fulfilled или false, если промис rejected.
  *
  * @param  {Array} arr - исходный массив
  * @returns {Array}
  */
-export const splitDataArray = (arr) => arr.map((subArr) => subArr.value);
+export const splitDataArray = (arr) =>
+  arr.map((item) => (item.status === 'fulfilled' ? item.value : false));
